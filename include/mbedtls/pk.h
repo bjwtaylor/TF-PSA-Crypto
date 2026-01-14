f89a184f015dcd60ff05ada6c3168132b5370bc8
Ben Taylor <ben.taylor@linaro.org>
2026-01-14 14:15:56 +0000

    Update code docs to reflect added support for wrapped RSA keys in check pair
    Signed-off-by: Ben Taylor <ben.taylor@linaro.org>

--- include/mbedtls/pk.h
+++ include/mbedtls/pk.h
@@ -247,8 +247,7 @@ void mbedtls_pk_restart_free(mbedtls_pk_restart_ctx *ctx);
  * The resulting context can only perform operations that are allowed by the
  * key's policy. Additionally, it currently has the following limitations:
  * - restartable operations can't be used;
- * - for RSA keys, signature verification is not supported, and neither is use
- *   of \c mbedtls_pk_check_pair().
+ * - for RSA keys, signature verification is not supported.
  *
  * \warning The PSA wrapped key must remain valid as long as the wrapping PK
  *          context is in use, that is at least between the point this function
@@ -711,9 +710,6 @@ int mbedtls_pk_sign_ext(mbedtls_pk_sigalg_t sig_type,
 
 /**
  * \brief           Check if a public-private pair of keys matches.
- *
- * \note            This function currently does not work on keys created with
- *                  \c mbedtls_pk_wrap_psa().
  *
  * \param pub       Context holding a public key.
  * \param prv       Context holding a private (and public) key.
